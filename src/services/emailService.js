c603168c286fa43a1da881dde631b26cc20b1934
277pawan <bpawan277@gmail.com>
2026-06-22 22:04:04 +0530

    refactor: code refacotr

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -5,9 +5,8 @@ const renderEmail = require("./renderEmail");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  family: 4,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
